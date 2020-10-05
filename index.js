0289985826fb31cf11c6eb0a5ad8b3455d4715d8
Elena Neroslavskaya <lenisha@gmail.com>
2020-10-05 16:54:36 -0400

    add pool

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 var appconfig = require('./appconfig');
 var express = require('express');
+const sql = require('mssql')
 
 var app = express();
-app.get('/', function (req, res) {
+app.get('/tedious', function (req, res) {
 
     console.log("Got request " + req.url + " to test add data to database via MSI AccessToken authentication.");
  
@@ -70,9 +71,73 @@ app.get('/', function (req, res) {
      } 
     );
 
+  
+
+});
+
+app.get('/pool',  function (req, res) {
+
+  console.log("Got request " + req.url + " to test add data to database via MSI AccessToken authentication.");
+
+  // connect to database
+  var config = {
+      server: appconfig.SQLSERVER,
+      database: appconfig.SQLDATABASE,
+      debug: true,
+      options: {
+        encrypt: true, //indicates if the connection should be encrypted
+        port: 1433, //port to establish connection
+        rowCollectionOnRequestCompletion: true, //returns rows object on the new Request callback
+        useColumnNames: true, //returns columns names within the rows object on the new Request callback
+        log: true,
+        enableArithAbort: true
+      },
+      authentication: {
+        type: "azure-active-directory-msi-vm" // "azure-active-directory-msi-app-service"
+        // optional - in K8S it's mapped using binding
+        //options: {
+        //  clientId: "0d6fd290-7c28-4ae2-88f4-78be54c0bab8"
+        //} 
+      }
+  };
+
+  console.log(" connect to database in request /pool with config: "+JSON.stringify(config));
+  fetchPool(config).then( results => {   
+    res.send( JSON.stringify(results.recordsets) );
+  })
 });
 
 
+
+
+async function fetchPool(config) {
+  try {
+    const pool = new sql.ConnectionPool(config);
+    const poolConnection = await pool.connect();
+   
+    poolConnection.on('debug', function(messagetext) {
+      if (messagetext) {
+          console.log('request database connection debugtext: ' +  JSON.stringify(messagetext));
+      }else {
+          console.log("request database connection Debug called with no messagetext object.");
+      }
+   });
+
+    const result = await poolConnection.request()
+          .query("SELECT * FROM [dbo].[CLOUD_ENG]");
+    
+    poolConnection.close();
+    console.log('Results = ' +  JSON.stringify(result));
+    console.log('Row count = ' + result.rowsAffected[0]);
+    return Promise.resolve(result);
+  }
+  catch(err) {
+    console.dir(err);
+    res.send( JSON.stringify(err) );
+    return Promise.reject(err);
+  }
+
+}
 
 function executeStatement(connection, req, res) {
   // If no error, then good to proceed.
